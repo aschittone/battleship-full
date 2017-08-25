c098dbe8e90893df5c45a884cc463c54d732a372
Luke Pierotti <lpierotti2@gmail.com>
2017-08-25 12:00:19 -0400

    final final

--- app/assets/javascripts/components/user.js
+++ app/assets/javascripts/components/user.js
@@ -5,7 +5,7 @@ class User {
     this.user_wins = userJson.user_wins
   }
   render() {
-    return `<center><h3 style="font-family: 'Black Ops One', cursive;"><li style="font-family: 'Black Ops One', cursive; text-align:left;">${this.user_name}- ${this.user_wins} wins</li></h3></center>`
+    return `<center><h3 style="font-family: 'Black Ops One', cursive;"><li style="font-family: 'Black Ops One', cursive; text-align:left;">${this.user_name}- ${this.user_wins} win(s)</li></h3></center>`
   }
 
 }
