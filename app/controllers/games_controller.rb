e1a79457ba9ca9eac69ba15ab42dfba30769244b
github username <github email address>
2017-10-12 18:48:47 -0400

    commiting to heroku

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def create
     winner = User.find_by(name: game_params[:winner])
