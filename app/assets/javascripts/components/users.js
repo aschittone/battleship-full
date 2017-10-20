2902a9f3836c7adf21126f92b8f5db5d06cf92c0
github username <github email address>
2017-10-20 16:36:39 -0400

    fix class name

--- app/assets/javascripts/components/users.js
+++ app/assets/javascripts/components/users.js
@@ -1,16 +1,16 @@
 class Users {
   constructor() {
     this.users = []
-    this.adapter = new UserAdapter()
+    this.adapter = new UsersAdapter()
     this.fetchAndLoadUsers()
   }
   fetchAndLoadUsers() {
     this.adapter.getUserHistory()
-    .then(usersJson => usersJson.forEach(user => this.users.push(new User(user) )))
-    .then(this.render.bind(this))
-    .catch((error) => console.log(error) )
+      .then(usersJson => usersJson.forEach(user => this.users.push(new User(user))))
+      .then(this.render.bind(this))
+      .catch((error) => console.log(error))
   }
-  userInfo(){
+  userInfo() {
     return this.users = this.users.map(user => user.render()).join('')
   }
 
